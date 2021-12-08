22c369feacbff72fa5da5d90e104b9b3c8dbf0ff
DARKnx <joao.vitornl@gmail.com>
2021-12-08 15:32:35 -0300

    atualizando docker

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ const PORT = 3333;
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+  console.log(`⚡️ [server]: Server is running at http://localhost:${PORT}`);
 });
+
